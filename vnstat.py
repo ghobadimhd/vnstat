e634f94f15dd21ff1c596e9b0a21592324ba96f9
mamad <ghobadi.mhd@gmail.com>
2017-01-05 17:37:13 +0330

    reformated_traffic added

--- vnstat.py
+++ vnstat.py
@@ -1,19 +1,44 @@
 from subprocess import getoutput
 import json
 import jalali
+from datetime import datetime
+import operator
 
-def traffic(interface='wlp3s0',date='p'):
-    """ """
+def traffic(interface='wlp3s0'):
+    """ it return a vnstat as json object """
     cmd = 'vnstat -i %s --json' % (interface,)
     vnstat_out = getoutput(cmd)
     vnstat_json = json.loads(vnstat_out)
-    traf_list = vnstat_json['interfaces'][0]['traffic']
-    if date == 'p':
-        for day in traf_list['days']:
-            day_date = '%s/%s/%s' % (day['date']['year'],day['date']['month'],day['date']['day'])
-            day['pdate'] = jalali.Gregorian(day_date).persian_string()
 
-    return traf_list
+
+    return vnstat_json
+
+def reformated_traffic():
+    """ reformat data
+
+    add persian (jalali) date
+    add date as object
+    sort data by date
+
+    """
+
+    data = traffic()
+    #print(data)
+    for interface in data['interfaces']:
+        for traffic_type in ['days', 'months', 'hours']:
+            for record in interface['traffic'][traffic_type]:
+                # if day not specified in date it replaced with 1
+                # usually it happens in month data
+                if 'day' not in record['date']: record['date']['day'] = 1
+
+                date_string = '%d/%d/%d' % (record['date']['year'],
+                                            record['date']['month'], record['date']['day'])
+                record['date'] = datetime(record['date']['year'],
+                                        record['date']['month'], record['date']['day'])
+                record['jdate'] = jalali.Gregorian(date_string)
+
+            interface['traffic'][traffic_type].sort(key=lambda x: x.get('date'))
+    return data
 
 
 def rx_sum(index='k'):
